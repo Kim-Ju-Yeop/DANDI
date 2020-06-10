dce9c317c3743af7d4510489f28148d5c33e8e9d
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-06-10 15:54:06 +0900

    [FIX] File

--- Dandi_Server/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	"extends": "airbnb-base",
-	"rules": {
-		"linebreak-style": ["error", "windows"],
-		"no-console": "off",
-		"no-param-reassign": "off",
-		"guard-for-in": "off",
-	}
-};
--- Dandi_Server/api/profile/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const tokenMiddleware = require('../../middleware/auth'); // middleware
-const imageCtrl = require('./profile.ctrl');
-
-router.get('/', tokenMiddleware, imageCtrl.getProfile);
-
-module.exports = router;
